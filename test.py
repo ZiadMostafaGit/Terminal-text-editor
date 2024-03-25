ebc8070a4b75f2e23586243a886ecf13c31a6865
ZiadMostafaGit <mygithub2003@gmail.com>
2024-03-25 15:25:49 +0200

    update

--- test.py
+++ test.py
@@ -2,12 +2,6 @@ import curses
 import argparse
 import sys
 
-
-
-
-
-# ...
-
 class Buffer:
     def __init__(self, lines):
         self.lines = lines
@@ -18,19 +12,13 @@ class Buffer:
     def __getitem__(self, index):
         return self.lines[index]
     
+    def insert(self, cur, k,buffer):
+        row, col = cur.row, cur.col
+        current = self.lines[row]
+        new = current[:col] + chr(k) + current[col:]
+        self.lines[row] = new
+        cur.move_right(buffer)
 
-    def insert(self,cur,k):
-        row,col=cur.row,cur.col
-        current=self.lines.pop(row)
-        new=current[:col]+k+current[col:]
-        self.lines.insert(row,new)
-
-# ... def main(stdscr):
-
-    
-
-    
-# ...
 
 class curser:
     def __init__(self, row, col,win ):
@@ -65,12 +53,6 @@ class curser:
             self.col=0
             self.move_down()    
 
-
-
-
-
-
-
     
     def move_tap_right(self, buffer):
         if self.col < len(buffer[self.row]):
@@ -85,42 +67,30 @@ class curser:
             self.col-=5
         else:
             self.col=len(buffer[self.row-1])
-            self.move_up()                
+            self.move_up()  
 
+class window:
+    def __init__(self, n_row, n_col):
+        self.n_row = n_row
+        self.n_col = n_col
 
-class  window:
-    def __init__(self,n_row,n_col) -> None:
-        self.n_row=n_row
-        self.n_col=n_col
-
-
-
-
-def Moving_Mode(k,cur,buffer):
-        if k == 27:  
-            sys.exit(0)
-        elif k == curses.KEY_LEFT: 
-            cur.move_left(buffer)
-        elif k == curses.KEY_RIGHT:
-            cur.move_right(buffer)
-        elif k == curses.KEY_UP:
-            cur.move_up()
-        elif k == curses.KEY_DOWN:
-            cur.move_down()
-        elif k==552:# the ctrl+left ascii
-            cur.move_tap_left(buffer)
-        elif k==567:#the ctrl +right ascii
-            cur.move_tap_right(buffer)   
-
-def Insert_Mode(stdscr,buffer,cur):
-    while True:
-                k=stdscr.getch()
-                if k==27:
-                    break
-                else:
-                    buffer.insert(cur,k)
-
-
+def Moving_Mode(k, cur, buffer):
+    if k == 27:  
+        sys.exit(0)
+    elif k == curses.KEY_LEFT: 
+        cur.move_left(buffer)
+    elif k == curses.KEY_RIGHT:
+        cur.move_right(buffer)
+    elif k == curses.KEY_UP:
+        cur.move_up()
+    elif k == curses.KEY_DOWN:
+        cur.move_down()
+    elif k == 552:
+        cur.move_tap_left(buffer)
+    elif k == 567:
+        cur.move_tap_right(buffer)
+    else:
+        buffer.insert(cur,k,buffer)    
 
 
 def main(stdscr):
@@ -133,36 +103,22 @@ def main(stdscr):
 
     curses.curs_set(1)
     win = window(curses.LINES - 1, curses.COLS - 1)
-    cur = curser(0, 0,win)
+    cur = curser(0, 0, win)
 
     while True:
         stdscr.erase()
-
 
         for row, line in enumerate(buffer[:win.n_row]):
             stdscr.addstr(row, 0, line[:win.n_col])
 
         stdscr.move(cur.row, cur.col)
         stdscr.refresh()
-       
-
 
         k = stdscr.getch()
         Moving_Mode(k, cur, buffer)
-        if k=="i":
-            Insert_Mode(stdscr,buffer,cur)
-
 
         stdscr.move(cur.row, cur.col)
         stdscr.refresh()
 
 if __name__ == "__main__":
     curses.wrapper(main)
-
-
-
-
-
-
-
-   
